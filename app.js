27a21a9f0c41b940253c0e3287b5dd75cabd9eda
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-14 01:56:14 +0530

    started

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const mongoose=require('mongoose')
+
+const app=express();
+
+// middle-ware 
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine','ejs')
+
+// database
+const dbURL='mongodb+srv://jacksonmintu:G9iPBBu5Dt0pIqzz@cluster0.k0qi4.mongodb.net/Auth_Node?retryWrites=true&w=majority';
+
+mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+.then((result)=>{
+  app.listen(3000);
+}).catch((err)=>console.log(err))
+
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const {Router}=require('express');
+const authController=require('../controllers/authController')
+
+
+const router=Router();
+
+router.get('/signup',()=>{})
+router.post('/signup',()=>{})
+router.get('/login',()=>{})
+router.post('/login',()=>{})
+
+module.exports=router;
