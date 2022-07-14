351b94f47a8de6f64fba6409b7a0cc56e892c67e
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-14 15:38:16 +0530

    mongoose valaidation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,26 @@
 const User=require('../models/User')
 
+const handleErrors=(err)=>{
+    console.log(err.message,err.code)
+    let errors={email:'',password:''};
 
+
+    // duplicate error code
+    if(err.code===11000)
+    {
+        errors.email='Email already exists'
+        return errors;
+    }
+
+    // validation errors
+    if(err.message.includes('user validation failed'))
+    {
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path]=properties.message;
+        })
+    }
+    return errors;
+}
 module.exports.signup_get=(req,res)=>{
     res.render('signup')
 }
@@ -14,8 +34,8 @@ module.exports.signup_post=async (req,res)=>{
         res.status(201).json(user)
     }
     catch(err){
-       
-        res.status(400).send('error user not created')
+      const errors= handleErrors(err)
+        res.status(400).json({errors})
     }
 }
 module.exports.login_post=(req,res)=>{
