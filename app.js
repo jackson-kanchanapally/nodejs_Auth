083bf71904eae4abf496f89239bfe1b2dcd3d173
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-15 14:37:52 +0530

    cookie examples removed

--- app.js
+++ app.js
@@ -28,18 +28,3 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
 
 
-// cookies
-app.get('/set-cookies',(req,res)=>{
-  // res.setHeader('Set-Cookie','newUser=true')
-  res.cookie('newUser',false)
-  res.cookie('isEmployee',true,{maxAge:1000*60*60*24,httpOnly:true}) //maxAge=cookie's expiry time
-  res.send('You got the cookies')
-})
-
-// read cookies
-app.get('/read-cookies',(req,res)=>{
-  const cookies=req.cookies;
-  console.log(cookies)
-
-  res.json(cookies)
-})
