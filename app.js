4aa1674f7698d7687ab4e725844961ce84e0189e
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-16 16:07:26 +0530

    checking current user and conditional rendering

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const authRoutes=require('./routes/authRoutes')
 const cookieParser=require('cookie-parser')
-const {requireAuth}=require('./middleware/authMiddleware')
+const {requireAuth,checkUser}=require('./middleware/authMiddleware')
 
 
 const app=express();
@@ -25,6 +25,7 @@ mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIn
 
 
 // routes
+app.get('*',checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth,(req, res) => res.render('smoothies'));
 app.use(authRoutes)
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt =require('jsonwebtoken')
+const User = require('../models/User');
 
 const requireAuth=(req,res,next)=>{
     const token=req.cookies.jwt;
@@ -21,4 +22,29 @@ const requireAuth=(req,res,next)=>{
     }
 }
 
-module.exports={requireAuth}
+const checkUser=(req,res,next)=>{
+    const token=req.cookies.jwt;
+
+    if(token)
+    {
+        jwt.verify(token,"jackson's secret",async (err,decodedToken)=>{
+            if(err)
+            {
+                console.log(err.message)
+                res.locals.user=null
+                next()
+            }
+            else{
+                let user=await User.findById(decodedToken.id)
+                res.locals.user=user
+                next()
+            }
+        })
+    }
+    else{
+        res.locals.user=null
+        next()
+    }
+}
+
+module.exports={requireAuth,checkUser}
