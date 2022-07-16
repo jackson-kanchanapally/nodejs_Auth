9b8738f44087c95985ebdd5c657a65e1da4ab420
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-16 14:48:03 +0530

    creating logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,6 @@ router.get('/signup',authController.signup_get)
 router.post('/signup',authController.signup_post)
 router.get('/login',authController.login_get)
 router.post('/login',authController.login_post)
+router.get('/logout',authContorller.logout_get)
 
 module.exports=router;
